6d1e7a9dd6937466fd5c245118af8e20b5beacb9
Nansi Uzunova <nansi.uzunova@imperiaonline.org>
2023-01-17 13:54:15 +0200

    Fixed deprecated classes

--- lib/main.dart
+++ lib/main.dart
@@ -50,9 +50,9 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Colors.green),
               child: Text(
                 'True',
                 style: TextStyle(
@@ -69,8 +69,8 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
               child: Text(
                 'False',
                 style: TextStyle(
